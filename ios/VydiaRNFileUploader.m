f0a71e4c64739c890157c586c26f48a1a43fae93
shiyansiva <35214422+shiyansiva@users.noreply.github.com>
2022-09-14 19:11:49 +0530

    Hide the session timeout
    Fixed connection time out issue in iOS

--- ios/VydiaRNFileUploader.m
+++ ios/VydiaRNFileUploader.m
@@ -434,7 +434,7 @@ RCT_EXPORT_METHOD(endBackgroundTask: (NSUInteger)taskId resolve:(RCTPromiseResol
 
             // UPDATE: Enforce a timeout here because we will otherwise
             // not get errors if the server times out
-            sessionConfiguration.timeoutIntervalForResource = 5 * 60;
+            // sessionConfiguration.timeoutIntervalForResource = 5 * 60;
 
             _urlSession = [NSURLSession sessionWithConfiguration:sessionConfiguration delegate:self delegateQueue:nil];
         }
